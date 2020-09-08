8641622608b58d822997233d74e2fcd936df29cf
hulltn <hulltn@mail.uc.edu>
2020-09-08 03:44:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.lang.ProcessBuilder.Redirect;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Count");
+    int totalMAndM;
+    double totalBlue;
+    double totalBrown;
+    double totalGreen;
+    double totalOrange;
+    double totalRed;
+    double totalYellow;
+    totalMAndM = 55*10;
+    totalBlue = totalMAndM *.24;
+    totalBrown = totalMAndM * .13;
+    totalGreen = totalMAndM * .16;
+    totalOrange = totalMAndM * .2;
+    totalRed = totalMAndM * .13;
+    totalYellow = totalMAndM *.14;
+    System.out.println("blue: " + totalBlue);
+    System.out.println("brown: " + totalBrown);
+    System.out.println("green: " + totalGreen);
+    System.out.println("orange: " + totalOrange);
+    System.out.println("red: " + totalRed);
+    System.out.println("yellow: " + totalYellow);
+    int sumMAandM;
+    sumMAandM = totalBlue + totalBrown + totalGreen + totalOrange + totalRed + totalYellow;
+    System.out.println("Sum: " + sumMAandM);
+    if (totalBlue > totalBrown && totalGreen > totalOrange)
+    System.out.println("Blue over Brown and Green over Orange");
+    if (totalBrown <= totalRed)
+    System.out.println ("Brown is less than or equal to Red");
+    if (sumMAandM = totalMAndM)
+    System.out.println ("Numbers match");
+    }
+}
